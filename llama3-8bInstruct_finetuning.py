3f0c462f59f38d8177c0a38b56872942910f4a7b
Naomi Greenberg <ngreenberg@luc.edu>
2024-10-30 10:31:00 -0500

    minor change

--- llama3-8bInstruct_finetuning.py
+++ llama3-8bInstruct_finetuning.py
@@ -42,7 +42,7 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of=8)
 
-model.config
+print(model.config)
 
 def clean_text(text):
     # replace newlines with spaces
