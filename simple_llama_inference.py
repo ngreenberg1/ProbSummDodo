25a18eeb29278ff4062a75239f468860478d6a4f
Naomi Greenberg <ngreenberg@luc.edu>
2024-11-08 12:58:14 -0600

    minor change

--- simple_llama_inference.py
+++ simple_llama_inference.py
@@ -13,8 +13,8 @@ pipe = pipeline(
 messages = [
     {"role": "system", "content": "You are a physician.  Please list as a semicolon separated list the most important problems/diagnoses based on the progress note text below. Only list the problems/diagnoses and nothing else. Be concise."},
     {"role": "user", "content": "H/O HYPERKALEMIA (HIGH POTASSIUM, HYPERPOTASSEMIA). H/O HYPERGLYCEMIA CHRONIC OBSTRUCTIVE PULMONARY DISEASE (COPD, BRONCHITIS, EMPHYSEMA) WITH ACUTE EXACERBATION. A 59 year-old man presents with malaise and hypoxia."},
-    {"role": "assistant", "content": "Hyperkalemia; Hyperglycemia; Chronic Obstructive Pulmonary Disease (COPD); Acute exacerbation of COPD; Hypoxia."},
-    {"role": "user", "content": "Think about the problem more, can you refine this list to the most important problems/diagnoses?"}
+    #{"role": "assistant", "content": "Hyperkalemia; Hyperglycemia; Chronic Obstructive Pulmonary Disease (COPD); Acute exacerbation of COPD; Hypoxia."},
+    #{"role": "user", "content": "Think about the problem more, can you refine this list to the most important problems/diagnoses?"}
 ]
 
 terminators = [
